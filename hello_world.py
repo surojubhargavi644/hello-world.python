bdbf207badfe611a43e37b5f12c2daa6e84a3299
Suroju Bhargavi <surojubhargavi644@gmail.com>
2026-01-15 20:06:32 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+# Taking user's name as input
+name = input("Suroju Bhargavi ")
+
+# Taking internship role as input
+role = input("Python Intern ")
+
+# Importing date module to get today's date
+from datetime import date
+today = date.today()
+
+# Printing the final output
+print("\n--- Details ---")
+print("Name:", name)
+print("Role:", role)
+print("Date:", today)
